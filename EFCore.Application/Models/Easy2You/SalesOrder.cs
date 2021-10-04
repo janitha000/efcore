aca0438661df1d690ba15f514950bf6a211a8b8b
Janitha Tennakoon <janithat@99x.io>
2021-10-04 09:38:45 +0530

    add jet

---
+++ EFCore.Application/Models/Easy2You/SalesOrder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore.Application.Models.Easy2You
+{
+    public class SalesOrder
+    {
+        public int Id { get; set; }
+        public List<SalesOrderAudit> SalesOrderAudits { get; set; }
+        public void AddAuditItem()
+        {
+            var audit = new SalesOrderAudit()
+            {
+                UpdatedAt = DateTime.UtcNow
+            };
+
+            SalesOrderAudits.Add(audit);
+            
+
+        }
+    }
+}
--- EFCore/Startup.cs
+++ EFCore/Startup.cs
@@ -45,6 +45,8 @@ namespace EFCore
 
             services.AddTransient<RequestConsoleLoggingMiddleware>();
 
+            services.AddCors();
+
             services.AddControllers()
                     .AddFluentValidation(f => f.RegisterValidatorsFromAssemblyContaining<Startup>())
                     .AddNewtonsoftJson(options =>
@@ -79,12 +81,12 @@ namespace EFCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EFCore v1"));
-            }
+            //}
 
             if(env.IsProduction())
             {
@@ -107,9 +109,15 @@ namespace EFCore
             app.UseRequestLogging();
 
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(builder => {
+                builder.AllowAnyOrigin();
+                builder.AllowAnyMethod();
+                builder.AllowAnyHeader();
+            });
 
             app.UseAuthentication();
 
